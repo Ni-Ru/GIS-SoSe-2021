947742d041c75562a07ce8c77a2e3f05c29cbe71
Nic <nic.rubner@hs-furtwangen.de>
2021-05-13 16:25:52 +0200

    Verbesserung 2.3

--- Kapitel_2/2.3/main.js
+++ Kapitel_2/2.3/main.js
@@ -1,2 +1,28 @@
 "use strict";
+var KlappBuch;
+(function (KlappBuch) {
+    /*let chosenVariable: FinalPlant = { //neue Variable, in der die Auswahl abgespeichert wird
+        blossom: null,
+        stem: null,
+        vase: null
+    }*/
+    function createDiv(_part) {
+        let option = document.createElement("div");
+        document.getElementById("selection").appendChild(option);
+        let img = document.createElement("img");
+        img.src = _part.imageUrl;
+        option.appendChild(img);
+        let button = document.createElement("button");
+        button.setAttribute("class", "choose");
+        option.appendChild(button);
+        button.innerHTML = "select";
+        button.addEventListener("click", event);
+        function event(_e) {
+            console.log(_part);
+        }
+    }
+    for (let i = 0; i < KlappBuch.partData.blossoms.length; i++) {
+        createDiv(KlappBuch.partData.blossoms[i]);
+    }
+})(KlappBuch || (KlappBuch = {}));
 //# sourceMappingURL=main.js.map
--- Kapitel_2/2.3/main.ts
+++ Kapitel_2/2.3/main.ts
@@ -1,35 +1,35 @@
 namespace KlappBuch{
 
-    export interface Plant{
-    plant: string;    
-    blossom: BlossomPart;
-    stem: StemPart;
-    vase: VasePart;
-    }
+    /*let chosenVariable: FinalPlant = { //neue Variable, in der die Auswahl abgespeichert wird
+        blossom: null,
+        stem: null,
+        vase: null
+    }*/
 
-    export interface Part{
-        part: string;
-        plant: string;
-        imageUrl: string;
-        width: number;
-        height: number;
-    }
+function createDiv(_part: Part):void{
 
-    export interface BlossomPart extends Part{
-        blossomed: boolean;
-        color: string;
-        smell: string;
-    }
+    let option: HTMLDivElement = document.createElement("div"); 
+    document.getElementById("selection").appendChild(option);
 
-    export interface VasePart extends Part{
-        color: string;
-        form: string;
-    }
+    let img: HTMLImageElement = document.createElement("img");
+    img.src = _part.imageUrl;
+    option.appendChild(img);
+    
+    let button: HTMLButtonElement= document.createElement("button");
+    button.setAttribute("class", "choose");
+    option.appendChild(button);
+    button.innerHTML="select";
+    button.addEventListener("click", event)
 
-    export interface StemPart extends Part{
-        thickness: string;
-        form: string;
-        thorns: boolean;
+    function event(_e: Event): void{
+        console.log(_part);
     }
+}
 
+for(let i = 0; i<partData.blossoms.length; i++){
+createDiv(partData.blossoms[i])
+}
+
+    
+    
 }
